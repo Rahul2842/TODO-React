496654fefb056f62be156950cd7869ce715ade89
Rahul Kumar <Rahulroy7078@gmail.com>
2023-10-01 16:41:46 +0530

    first commit

---
+++ MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia et ab numquam autem, fugiat eos nesciunt architecto saepe debitis facilis.</p>
+        </div>
+    )
+}
